1db99abd5bae5395638c28887f2f8e680daf407e
xps-admin <strategicmindsadvisory@gmail.com>
2026-06-22 16:36:05 -0400

    fix: v4.0.1 — auto-validate import fix

--- lib/build-info.ts
+++ lib/build-info.ts
@@ -1,4 +1,2 @@
-export const BUILD_TIME = "2026-06-22T20:33:06.672904";
-export const VERSION = "4.0.0";
-export const AUTO_LOOP = true;
-export const CRONS = 10;
+export const BUILD_TIME = "2026-06-22T20:36:05.320709";
+export const VERSION = "4.0.1";
